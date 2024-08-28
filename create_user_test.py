8541b703505f8e280b212378d2b94d98692a4008
Alvaro Valdes <101993890+Alvaro12v@users.noreply.github.com>
2024-08-28 03:50:44 +0100

    mi primer commit

--- create_user_test.py
+++ create_user_test.py
@@ -136,3 +136,5 @@ def test_create_user_number_type_first_name_get_error_response():
     user_body = get_user_body(12)
     response = sender_stand_request.post_new_user(user_body)
     assert response.status_code == 400
+
+C:\python_pycharm\api_stand_tests
